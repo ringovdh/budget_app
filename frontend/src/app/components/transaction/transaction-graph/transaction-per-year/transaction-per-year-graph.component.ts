9be45e76aa491992e8fd5bda9cddf59506017c44
Van den Hende Ringo <>
2021-11-11 21:16:01 +0100

    add tx per year graph

---
+++ frontend/src/app/components/transaction/transaction-graph/transaction-per-year/transaction-per-year-graph.component.ts
@@ -0,0 +1,83 @@
+import {Component, Input, OnChanges} from '@angular/core';
+import { Chart } from 'chart.js';
+import {Transaction} from "../../../../model/transaction";
+
+@Component({
+  selector: 'app-transaction-per-year-graph',
+  templateUrl: './transaction-per-year-graph.component.html',
+  styleUrls: ['./transaction-per-year-graph.component.css']
+})
+export class TransactionPerYearGraphComponent implements OnChanges {
+
+  @Input() transactions: Transaction[];
+  polarAreaChart: Chart;
+
+  constructor() { }
+
+  ngOnChanges() {
+    if (this.transactions != null) {
+      let amounts = new Array();
+      let categories = new Array();
+      const groups = this.groupAndCountPerCategory();
+
+      for (const key in groups) {
+        categories.push(key)
+        amounts.push(groups[key])
+      }
+      if (this.polarAreaChart != null) {
+        this.polarAreaChart.destroy();
+      }
+      this.polarAreaChart = new Chart('polarAreaChartYears', {
+        type: 'bar', data: {
+          labels: categories,
+          datasets: [
+            { data: amounts,
+              borderColor: '#303e45',
+              backgroundColor: "#7d97a5",
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      });
+    }
+  }
+
+  public groupAndCountPerCategory() {
+    const group = new Array();
+    this.transactions.forEach(function(transaction) {
+      const cat = transaction.category.label;
+console.log("filter")
+      //TODO remove hardcoded id's!
+      if (transaction.category.id != 44 && transaction.category.id != 27 && transaction.category.id != 26) {
+        if (cat in group) {
+          if (transaction.sign == '-') {
+            group[cat] = group[cat] - transaction.amount;
+          } else {
+            group[cat] = group[cat] + transaction.amount;
+          }
+        } else {
+          if (transaction.sign == '-') {
+            group[cat] = 0-transaction.amount;
+          } else {
+            group[cat] = transaction.amount;
+          }
+        }
+      }
+    });
+
+    return group;
+  }
+
+}
